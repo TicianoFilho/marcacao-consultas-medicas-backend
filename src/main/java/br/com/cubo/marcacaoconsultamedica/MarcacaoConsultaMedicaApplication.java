1f05b6d7592821532252da55079a0ecf9a22027a
TicianoFilho <ticianofilho@gmail.com>
2022-12-17 15:53:50 -0300

    build: create project

---
+++ src/main/java/br/com/cubo/marcacaoconsultamedica/MarcacaoConsultaMedicaApplication.java
@@ -0,0 +1,13 @@
+package br.com.cubo.marcacaoconsultamedica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarcacaoConsultaMedicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarcacaoConsultaMedicaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/cubo/marcacaoconsultamedica/MarcacaoConsultaMedicaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cubo.marcacaoconsultamedica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarcacaoConsultaMedicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
