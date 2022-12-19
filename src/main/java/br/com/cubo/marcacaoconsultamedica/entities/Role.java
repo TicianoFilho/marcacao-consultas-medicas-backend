3949f04bc70888a9cdb1bacca592709a25cbfaf2
TicianoFilho <ticianofilho@gmail.com>
2022-12-19 18:19:54 -0300

    refactor: implement Role class

--- src/main/java/br/com/cubo/marcacaoconsultamedica/entities/Role.java
+++ src/main/java/br/com/cubo/marcacaoconsultamedica/entities/Role.java
@@ -1,5 +1,29 @@
 package br.com.cubo.marcacaoconsultamedica.entities;
 
-public class Role {
+import java.io.Serializable;
+import java.util.UUID;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "ROLE")
+public class Role implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private UUID id;
+	
+	@Column(nullable = false, unique = true)
+	private String descricao;
 }
