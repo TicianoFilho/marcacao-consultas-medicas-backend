8607205ce832a7cf6799a3e7365010cba52de0cc
TicianoFilho <ticianofilho@gmail.com>
2022-12-20 18:42:50 -0300

    refactor: set relationship with roles as eager

--- src/main/java/br/com/cubo/marcacaoconsultamedica/entities/login/Usuario.java
+++ src/main/java/br/com/cubo/marcacaoconsultamedica/entities/login/Usuario.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -31,7 +32,7 @@ public class Usuario {
 	@Column(nullable = false)
 	private String password;
 	
-	@ManyToMany
+	@ManyToMany(fetch = FetchType.EAGER)
 	@JoinTable(
 			name = "usuarios_roles",
 			joinColumns = @JoinColumn(name = "usuario_id"),
