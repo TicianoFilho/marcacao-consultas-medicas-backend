03481d092fc0e9a7fb030a2e71560eaafa057d3e
TicianoFilho <ticianofilho@gmail.com>
2022-12-20 20:06:17 -0300

    create: add UserDetailsServiceImpl. refactor SecurityConfig class

--- src/main/java/br/com/cubo/marcacaoconsultamedica/security/SecurityConfig.java
+++ src/main/java/br/com/cubo/marcacaoconsultamedica/security/SecurityConfig.java
@@ -2,11 +2,16 @@ package br.com.cubo.marcacaoconsultamedica.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -14,11 +19,18 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
+	private final UserDetailsServiceImpl userDetailService;
+	
+	public SecurityConfig(UserDetailsServiceImpl userDetailService) {
+		this.userDetailService = userDetailService;
+	}
+
 	@Bean
 	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http
 			.csrf().disable()
 			.authorizeRequests()
+			.antMatchers(HttpMethod.GET).authenticated()
 			.anyRequest().authenticated()
 			.and()
 			.httpBasic();
@@ -40,5 +52,15 @@ public class SecurityConfig {
 				.build();
 		
 		return new InMemoryUserDetailsManager(admin, user);
+	}
+	
+	@Bean
+	AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+		return authenticationConfiguration.getAuthenticationManager();
+	}
+	
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 }
---
+++ src/main/java/br/com/cubo/marcacaoconsultamedica/security/UserDetailsServiceImpl.java
@@ -0,0 +1,38 @@
+package br.com.cubo.marcacaoconsultamedica.security;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import br.com.cubo.marcacaoconsultamedica.entities.login.Role;
+import br.com.cubo.marcacaoconsultamedica.entities.login.Usuario;
+import br.com.cubo.marcacaoconsultamedica.repositories.UsuarioRepository;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+	
+	private final UsuarioRepository usuarioRepository;
+	
+	public UserDetailsServiceImpl(UsuarioRepository usuarioRepository) {
+		this.usuarioRepository = usuarioRepository;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		Usuario usuario = usuarioRepository.findByUsername(username).orElseThrow(
+				() -> new UsernameNotFoundException(String.format("Usuário %S não encontrado.", username)));
+		return new User(usuario.getUsername(), usuario.getPassword(), mapRolesToAuthorities(usuario.getRoles())); 
+	}
+
+	// turn List<Roles> into Collection<grantedAuthority> 
+	private Collection<GrantedAuthority> mapRolesToAuthorities(List<Role> roles) {
+		return roles.stream().map(role -> new SimpleGrantedAuthority(role.getDescricao())).collect(Collectors.toList());
+	}
+	
+}
