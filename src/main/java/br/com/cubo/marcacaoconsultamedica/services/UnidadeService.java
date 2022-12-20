91fb89f4bd83544e478f62a9f76ad8613e0d9403
TicianoFilho <ticianofilho@gmail.com>
2022-12-20 15:38:18 -0300

    create: add UnidadeService interface

---
+++ src/main/java/br/com/cubo/marcacaoconsultamedica/services/UnidadeService.java
@@ -0,0 +1,11 @@
+package br.com.cubo.marcacaoconsultamedica.services;
+
+import java.util.List;
+
+import br.com.cubo.marcacaoconsultamedica.entities.Unidade;
+
+public interface UnidadeService {
+
+	public List<Unidade> unidades();
+	
+}
