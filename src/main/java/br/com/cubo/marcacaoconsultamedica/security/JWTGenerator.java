9903ffef3cc4ec0e82871457eeb53ee160619356
TicianoFilho <ticianofilho@gmail.com>
2022-12-22 18:21:35 -0300

    create: add JWTGenerator SecurityConstants classes. Generate token ok

---
+++ src/main/java/br/com/cubo/marcacaoconsultamedica/security/JWTGenerator.java
@@ -0,0 +1,48 @@
+package br.com.cubo.marcacaoconsultamedica.security;
+
+import java.util.Date;
+
+import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+@Component
+public class JWTGenerator {
+
+	public String generateToken(Authentication authentication) {
+		
+		String username = authentication.getName();
+		Date expireDate = new Date(new Date().getTime() + SecurityConstants.JWT_EXPIRATION);     // current date + token expiration time.
+		
+		String token = Jwts.builder()
+				.setSubject(username)
+				.setIssuedAt(new Date())
+				.setExpiration(expireDate)
+				.signWith(SignatureAlgorithm.HS512, SecurityConstants.JWT_SECRET)
+				.compact();
+		
+		return token;
+	} 
+	
+	public String getUsernameFromJWT(String token) {
+		Claims claims = Jwts.parser()
+				.setSigningKey(SecurityConstants.JWT_SECRET)
+				.parseClaimsJws(token)
+				.getBody();
+		
+		return claims.getSubject();
+	}
+	
+	public boolean validateToken(String token) {
+		try {
+			Jwts.parser().setSigningKey(SecurityConstants.JWT_SECRET).parseClaimsJws(token);
+			return true;
+		} catch (Exception e) {
+			throw new AuthenticationCredentialsNotFoundException("JWT expirado ou incorreto.");
+		}
+	}
+}
---
+++ src/main/java/br/com/cubo/marcacaoconsultamedica/security/SecurityConstants.java
@@ -0,0 +1,7 @@
+package br.com.cubo.marcacaoconsultamedica.security;
+
+public class SecurityConstants {
+
+	public static final long JWT_EXPIRATION = 70000L;	
+	public static final String JWT_SECRET = "cb9c33a5-ddbf-4729-a09f-6b0c172f2ede";
+}
