e2176b1eb5975ac5bc21c5d9e91d66fd22804b00
TicianoFilho <ticianofilho@gmail.com>
2022-12-23 18:30:01 -0300

    create: add MedicoService

--- src/main/java/br/com/cubo/marcacaoconsultamedica/services/MedicoService.java
+++ src/main/java/br/com/cubo/marcacaoconsultamedica/services/MedicoService.java
@@ -1,5 +1,17 @@
 package br.com.cubo.marcacaoconsultamedica.services;
 
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import br.com.cubo.marcacaoconsultamedica.entities.Medico;
+
 public interface MedicoService {
 
+	Medico save(Medico medico);
+	Page<Medico> findAll(Pageable pageable);
+	Optional<Medico> findOneById(UUID id);
+	void delete(Medico medico);	
 }
