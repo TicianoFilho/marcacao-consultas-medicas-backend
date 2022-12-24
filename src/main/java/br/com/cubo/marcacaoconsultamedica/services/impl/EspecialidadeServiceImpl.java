ff8aaa90593f3e4a111dac2b57747ff236febc9d
TicianoFilho <ticianofilho@gmail.com>
2022-12-24 17:20:30 -0300

    refactor: implementation of EspecialidadeServiceImpl

--- src/main/java/br/com/cubo/marcacaoconsultamedica/services/impl/EspecialidadeServiceImpl.java
+++ src/main/java/br/com/cubo/marcacaoconsultamedica/services/impl/EspecialidadeServiceImpl.java
@@ -3,35 +3,45 @@ package br.com.cubo.marcacaoconsultamedica.services.impl;
 import java.util.Optional;
 import java.util.UUID;
 
+import javax.transaction.Transactional;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
 
 import br.com.cubo.marcacaoconsultamedica.entities.Especialidade;
+import br.com.cubo.marcacaoconsultamedica.repositories.EspecialidadeRepository;
 import br.com.cubo.marcacaoconsultamedica.services.EspecialidadeService;
 
+@Service
 public class EspecialidadeServiceImpl implements EspecialidadeService {
 
+	private final EspecialidadeRepository especialidadeRepository;
+	
+	public EspecialidadeServiceImpl(EspecialidadeRepository especialidadeRepository) {
+		this.especialidadeRepository = especialidadeRepository;
+	}
+
 	@Override
+	@Transactional
 	public Especialidade save(Especialidade especialidade) {
-		// TODO Auto-generated method stub
-		return null;
+		return especialidadeRepository.save(especialidade);
 	}
 
 	@Override
 	public Page<Especialidade> findAll(Pageable pageable) {
-		// TODO Auto-generated method stub
-		return null;
+		return especialidadeRepository.findAll(pageable);
 	}
 
 	@Override
 	public Optional<Especialidade> findOneById(UUID id) {
-		// TODO Auto-generated method stub
-		return Optional.empty();
+		return especialidadeRepository.findById(id);
 	}
 
 	@Override
+	@Transactional
 	public void delete(Especialidade especialidade) {
-		// TODO Auto-generated method stub
+		especialidadeRepository.delete(especialidade);
 		
 	}
 
