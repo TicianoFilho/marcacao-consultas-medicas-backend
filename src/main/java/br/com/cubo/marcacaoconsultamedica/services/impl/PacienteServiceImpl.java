d46989619c54c43387bdddc3cb3a7ecf30ec50f6
TicianoFilho <ticianofilho@gmail.com>
2022-12-29 20:06:10 -0300

    create: add PacienteServiceImpl

---
+++ src/main/java/br/com/cubo/marcacaoconsultamedica/services/impl/PacienteServiceImpl.java
@@ -0,0 +1,44 @@
+package br.com.cubo.marcacaoconsultamedica.services.impl;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import br.com.cubo.marcacaoconsultamedica.entities.Paciente;
+import br.com.cubo.marcacaoconsultamedica.repositories.PacienteRepository;
+import br.com.cubo.marcacaoconsultamedica.services.PacienteService;
+
+@Service
+public class PacienteServiceImpl implements PacienteService {
+	
+	private final PacienteRepository pacienteRepository;
+
+	public PacienteServiceImpl(PacienteRepository pacienteRepository) {
+		this.pacienteRepository = pacienteRepository;
+	}
+
+	@Override
+	public Paciente save(Paciente paciente) {
+		return pacienteRepository.save(paciente);
+	}
+
+	@Override
+	public Page<Paciente> findAll(Pageable pageable) {
+		return pacienteRepository.findAll(pageable);
+	}
+
+	@Override
+	public Optional<Paciente> findOneById(UUID id) {
+		return pacienteRepository.findById(id);
+	}
+
+	@Override
+	public void delete(Paciente paciente) {
+		pacienteRepository.delete(paciente);
+		
+	}
+
+}
