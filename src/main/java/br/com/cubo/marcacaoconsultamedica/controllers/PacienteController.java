449c70bab129154966e89a44d7642cd75d44c37f
TicianoFilho <ticianofilho@gmail.com>
2022-12-29 20:54:16 -0300

    feat: add findAllPaciente method in PacienteController

--- src/main/java/br/com/cubo/marcacaoconsultamedica/controllers/PacienteController.java
+++ src/main/java/br/com/cubo/marcacaoconsultamedica/controllers/PacienteController.java
@@ -1,8 +1,16 @@
 package br.com.cubo.marcacaoconsultamedica.controllers;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.cubo.marcacaoconsultamedica.entities.Medico;
+import br.com.cubo.marcacaoconsultamedica.entities.Paciente;
 import br.com.cubo.marcacaoconsultamedica.services.PacienteService;
 
 @RestController
@@ -15,5 +23,10 @@ public class PacienteController {
 		this.pacienteService = pacienteService;
 	}
 	
+	@GetMapping
+	public ResponseEntity<Page<Paciente>> getAllPacientes(
+			@PageableDefault(page = 0, size = 5, sort = "id", direction = Direction.ASC) Pageable pageable) {
+		return ResponseEntity.ok().body(pacienteService.findAll(pageable));		
+	}
 	
 }
