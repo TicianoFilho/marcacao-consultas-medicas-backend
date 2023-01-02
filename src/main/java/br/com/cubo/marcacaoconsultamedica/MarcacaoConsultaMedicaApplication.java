fa130939b2b1c5c6b70ec3534a6a4cc4e43ec968
TicianoFilho <ticianofilho@gmail.com>
2023-01-02 19:45:27 -0300

    docs: add security to the documentation

--- src/main/java/br/com/cubo/marcacaoconsultamedica/MarcacaoConsultaMedicaApplication.java
+++ src/main/java/br/com/cubo/marcacaoconsultamedica/MarcacaoConsultaMedicaApplication.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
@@ -16,6 +18,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 				description = "A aplicação realiza agendamentos/marcações de consultas médicas"),
 		servers = @Server(url = "http://localhost:8080"),
 		tags = {@Tag(name = "Unidade", description = "Local onde será realizado o atendimento médico agendado.")})
+@SecurityScheme(name = "BearerJWT", type = SecuritySchemeType.HTTP, scheme = "bearer", bearerFormat = "JWT", description = "Gera o token para o projeto.")
 public class MarcacaoConsultaMedicaApplication {
 
 	public static void main(String[] args) {
